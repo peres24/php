1abe5c945711dfe5e8d0f885c1d0214240df4ea8
peres24 <davidperesfilho24@gmail.com>
2026-03-24 14:56:07 -0300

    primeiros 2 exercicios prontos

---
+++ lista1/exer1.php
@@ -0,0 +1,5 @@
+<?php
+$nomes = ["David", "Heder", "Miguel", "Teixeira", "Heitor"];
+echo 'Primeiro nome: '.$nomes[0].'<br>';
+echo 'Ultimo nome: '.$nomes[4];
+?>
---
+++ lista1/exer2.php
@@ -0,0 +1,8 @@
+<?php
+$valores = [ 
+    "nome" => "Farinha",
+    "preco" => 5,
+    "estoque" => 67
+];
+echo 'O produto '.$valores["nome"].' custa '.$valores["preco"].' R$';
+?>
