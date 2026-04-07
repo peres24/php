019aa5937ebe0e7ed861255a09918034e12fdada
peres24 <davidperesfilho24@gmail.com>
2026-04-07 08:43:26 -0300

    terminei o 3 e comeco da aula 4

---
+++ exercicios-arnaldo1/aula3/exer2/includes/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<title>Sistema Financeiro</title>
+https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css
+</head>
+<body>
---
+++ exercicios-arnaldo1/aula4/exer3.php
@@ -0,0 +1,14 @@
+<?php
+$usuarios = [
+    [
+        "usuario" => "David",
+        "senha" =>  "senha4321",
+        "perfil" => "privado"
+    ],
+    [
+        "usuario" => "Dragao09",
+        "senha" => "senha1234",
+        "saldo" => "publico"
+    ]
+    ];
+?>
